8df4ba673fb8a74f2137cbacc5949a179575d385
Rick Waldron <waldron.rick@gmail.com>
2020-03-27 14:12:55 -0400

    URL Preview Block: simplify preview parsing and url construction

--- frontend/index.js
+++ frontend/index.js
@@ -223,124 +223,98 @@ function getPreviewUrlForCellValue(url) {
 
     // Try to extract the preview URL from the URL using regular expression
     // based helper functions for each service we support.
-    const airtablePreviewUrl = getAirtablePreviewUrl(url);
-    if (airtablePreviewUrl) {
-        return airtablePreviewUrl;
+    //
+    for (const converter of converters) {
+        const previewUrl = converter(url);
+        if (previewUrl) {
+            return previewUrl;
+        }
     }
-
-    const youtubePreviewUrl = getYoutubePreviewUrl(url);
-    if (youtubePreviewUrl) {
-        return youtubePreviewUrl;
-    }
-
-    const vimeoPreviewUrl = getVimeoPreviewUrl(url);
-    if (vimeoPreviewUrl) {
-        return vimeoPreviewUrl;
-    }
-
-    const spotifyPreviewUrl = getSpotifyPreviewUrl(url);
-    if (spotifyPreviewUrl) {
-        return spotifyPreviewUrl;
-    }
-
-    const soundcloudPreviewUrl = getSoundcloudPreviewUrl(url);
-    if (soundcloudPreviewUrl) {
-        return soundcloudPreviewUrl;
-    }
-
-    const figmaPreviewUrl = getFigmaPreviewUrl(url);
-    if (figmaPreviewUrl) {
-        return figmaPreviewUrl;
-    }
-
-    // URL didn't match any supported services, so return null
+    // If no converter is found, return null.
     return null;
 }
 
-function getAirtablePreviewUrl(url) {
-    const match = url.match(/airtable\.com(\/embed)?\/(shr[A-Za-z0-9]{14}.*)/);
-    if (match) {
-        return `https://airtable.com/embed/${match[2]}`;
-    }
+const converters = [
+    url => {
+        const match = url.match(/airtable\.com(\/embed)?\/(shr[A-Za-z0-9]{14}.*)/);
+        if (match) {
+            return `https://airtable.com/embed/${match[2]}`;
+        }
 
-    // URL isn't for an Airtable share
-    return null;
-}
+        // URL isn't for an Airtable share
+        return null;
+    },
+    url => {
+        // Standard youtube urls, e.g. https://www.youtube.com/watch?v=KYz2wyBy3kc
+        let match = url.match(/youtube\.com\/.*v=([\w-]+)(&|$)/);
 
-function getYoutubePreviewUrl(url) {
-    // Standard youtube urls, e.g. https://www.youtube.com/watch?v=KYz2wyBy3kc
-    let match = url.match(/youtube\.com\/.*v=([\w-]+)(&|$)/);
+        if (match) {
+            return `https://www.youtube.com/embed/${match[1]}`;
+        }
 
-    if (match) {
-        return `https://www.youtube.com/embed/${match[1]}`;
-    }
+        // Shortened youtube urls, e.g. https://youtu.be/KYz2wyBy3kc
+        match = url.match(/youtu\.be\/([\w-]+)(\?|$)/);
+        if (match) {
+            return `https://www.youtube.com/embed/${match[1]}`;
+        }
 
-    // Shortened youtube urls, e.g. https://youtu.be/KYz2wyBy3kc
-    match = url.match(/youtu\.be\/([\w-]+)(\?|$)/);
-    if (match) {
-        return `https://www.youtube.com/embed/${match[1]}`;
-    }
+        // Youtube playlist urls, e.g. youtube.com/playlist?list=KYz2wyBy3kc
+        match = url.match(/youtube\.com\/playlist\?.*list=([\w-]+)(&|$)/);
+        if (match) {
+            return `https://www.youtube.com/embed/videoseries?list=${match[1]}`;
+        }
 
-    // Youtube playlist urls, e.g. youtube.com/playlist?list=KYz2wyBy3kc
-    match = url.match(/youtube\.com\/playlist\?.*list=([\w-]+)(&|$)/);
-    if (match) {
-        return `https://www.youtube.com/embed/videoseries?list=${match[1]}`;
-    }
+        // URL isn't for a youtube video
+        return null;
+    },
+    url => {
+        const match = url.match(/vimeo\.com\/([\w-]+)(\?|$)/);
+        if (match) {
+            return `https://player.vimeo.com/video/${match[1]}`;
+        }
 
-    // URL isn't for a youtube video
-    return null;
-}
+        // URL isn't for a Vimeo video
+        return null;
+    },
+    url => {
+        // Spotify URLs for song, album, artist, playlist all have similar formats
+        let match = url.match(/spotify\.com\/(track|album|artist|playlist)\/([\w-]+)(\?|$)/);
+        if (match) {
+            return `https://open.spotify.com/embed/${match[1]}/${match[2]}`;
+        }
 
-function getVimeoPreviewUrl(url) {
-    const match = url.match(/vimeo\.com\/([\w-]+)(\?|$)/);
-    if (match) {
-        return `https://player.vimeo.com/video/${match[1]}`;
-    }
+        // Spotify URLs for podcasts and episodes have a different format
+        match = url.match(/spotify\.com\/(show|episode)\/([\w-]+)(\?|$)/);
+        if (match) {
+            return `https://open.spotify.com/embed-podcast/${match[1]}/${match[2]}`;
+        }
 
-    // URL isn't for a Vimeo video
-    return null;
-}
+        // URL isn't for Spotify
+        return null;
+    },
+    url => {
+        // Soundcloud url's don't have a clear format, so just check if they are from soundcloud and try
+        // to embed them.
+        if (url.match(/soundcloud\.com/)) {
+            return `https://w.soundcloud.com/player/?url=${url}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`;
+        }
 
-function getSpotifyPreviewUrl(url) {
-    // Spotify URLs for song, album, artist, playlist all have similar formats
-    let match = url.match(/spotify\.com\/(track|album|artist|playlist)\/([\w-]+)(\?|$)/);
-    if (match) {
-        return `https://open.spotify.com/embed/${match[1]}/${match[2]}`;
-    }
+        // URL isn't for Soundcloud
+        return null;
+    },
+    url => {
+        // Figma has a regex they recommend matching against
+        if (
+            url.match(
+                /(https:\/\/([\w.-]+\.)?)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/,
+            )
+        ) {
+            return `https://www.figma.com/embed?embed_host=astra&url=${url}`;
+        }
 
-    // Spotify URLs for podcasts and episodes have a different format
-    match = url.match(/spotify\.com\/(show|episode)\/([\w-]+)(\?|$)/);
-    if (match) {
-        return `https://open.spotify.com/embed-podcast/${match[1]}/${match[2]}`;
-    }
-
-    // URL isn't for Spotify
-    return null;
-}
-
-function getSoundcloudPreviewUrl(url) {
-    // Soundcloud url's don't have a clear format, so just check if they are from soundcloud and try
-    // to embed them.
-    if (url.match(/soundcloud\.com/)) {
-        return `https://w.soundcloud.com/player/?url=${url}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`;
-    }
-
-    // URL isn't for Soundcloud
-    return null;
-}
-
-function getFigmaPreviewUrl(url) {
-    // Figma has a regex they recommend matching against
-    if (
-        url.match(
-            /(https:\/\/([\w.-]+\.)?)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/,
-        )
-    ) {
-        return `https://www.figma.com/embed?embed_host=astra&url=${url}`;
-    }
-
-    // URL isn't for Figma
-    return null;
-}
+        // URL isn't for Figma
+        return null;
+    },
+];
 
 initializeBlock(() => <UrlPreviewBlock />);
